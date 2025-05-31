75198961995b5831a36d1728eee68ff272995673
aion0721 <rudolf.aion@gmail.com>
2025-06-01 00:05:58 +0900

    feat: add bonus

---
+++ SampleCode/gisho18-bonus/1.1/my-app/src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import type { TodoType } from "../types/todo";
+
+const ulStyle = {
+  listStyle: "none",
+  padding: "0",
+};
+
+const liStyle = {
+  padding: "0.5em 0",
+  borderBottom: "1px solid #eee",
+};
+
+/** TodoList に渡す props */
+type TodoListProps = {
+  /** 表示するTodoのリスト */
+  todos: TodoType[];
+};
+
+/**
+ * Todoの一覧を表示するコンポーネント
+ */
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  return (
+    <ul style={ulStyle}>
+      {todos.map((todo) => (
+        <li style={liStyle} key={todo.id}>
+          {todo.title} [{todo.status}]
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
